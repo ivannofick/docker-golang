2e3a8491977ad66c95b410a4490717cc014fcd23
ivannofick <ivannofick@gmail.com>
2022-11-07 14:32:39 +0700

    change table

--- go-app/tentangkodego_jwt_mux/models/setup.go
+++ go-app/tentangkodego_jwt_mux/models/setup.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	db, err := gorm.Open(mysql.Open("root:@tcp(localhost:3306)/go_jwt_mux"))
+	db, err := gorm.Open(mysql.Open("root:@tcp(localhost:3306)/api_golang"))
 	if err != nil {
 		fmt.Println("Gagal koneksi database")
 	}
